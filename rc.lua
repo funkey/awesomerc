6e8e2f9bcb7fa06cb38e19ab42ab6208623b6756
Jan Funke <jfunke@iri.upc.edu>
2016-05-10 11:53:41 +0200

    went back to gnome-terminal

--- rc.lua
+++ rc.lua
@@ -54,7 +54,7 @@ end
 beautiful.init("~/.config/awesome/themes/current/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "x-terminal-emulator"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
