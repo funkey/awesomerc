2ffeb027c657019c6702b15c13731e8b4b7cbb27
Jan Funke <jfunke@iri.upc.edu>
2016-05-11 13:04:09 +0200

    re-enabled autostart

--- rc.lua
+++ rc.lua
@@ -517,4 +517,4 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
---require("autostart")
+require("autostart")
