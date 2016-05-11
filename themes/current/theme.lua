4b10985a08cd5589d51bd19b9b461a793f3fed8c
Jan Funke <jfunke@iri.upc.edu>
2016-05-11 16:21:43 +0200

    set default font to "sans bold 11" (still can't get dejavu)

--- themes/current/theme.lua
+++ themes/current/theme.lua
@@ -2,7 +2,8 @@ theme            = {}
 theme.icons      = os.getenv("HOME") .. "/.config/awesome/themes/current/icons/"
 theme.wallpaper  = os.getenv("HOME") .. "/.config/awesome/themes/current/wallpapers/wallpaper.jpg"
 theme.panel      = "png:" .. theme.icons .. "/panel/panel.png"
-theme.font       = "DejaVu Sans 9" -- I'd like to have DejaVu, but it doesn't work
+--theme.font       = "DejaVu Sans 9" -- I'd like to have DejaVu, but it doesn't work
+theme.font       = "sans bold 11"
 
 theme.color_black   = "#111111"
 theme.color_gray    = "#555555"
